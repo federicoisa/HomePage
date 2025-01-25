73edbdfeececb8456fabc0ceb6eaad997d244d0a
Federico Isa <fedeisa195@gmail.com>
2025-01-25 17:31:17 -0300

    ubdate image

--- Pages/HomePage.jsx
+++ Pages/HomePage.jsx
@@ -76,7 +76,7 @@ export const HomePage = () => {
 
           <section>
             <div className="image">
-              <img src="/images/foto fede.jpg" alt="Foto Perfil Portfolio" />
+             <img src="/Portfolio-FedericoIsa/images/foto-fede.jpg" alt="Foto Fede" />
             </div>
           </section>
         </div>
