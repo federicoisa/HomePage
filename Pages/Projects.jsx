276cd91b90d851775531e61af909fa937d30a57e
Federico Isa <fedeisa195@gmail.com>
2025-01-25 20:50:07 -0300

    v10.0.0

--- Pages/Projects.jsx
+++ Pages/Projects.jsx
@@ -1,4 +1,4 @@
-
+import { Link } from 'react-router-dom';
 
 export const Projects = () => {
     
@@ -9,7 +9,7 @@ export const Projects = () => {
         <div className="flex-container">
           {/* Tarjetas de proyectos */}
             <div className="card">
-              <a href="/HTML/AcerdaDe.html">
+              <Link to="/HTML/AcerdaDe.html">
                 <div className="image-container">
                   <img
                     src="/images/gifExpertApp2.png"
@@ -18,7 +18,7 @@ export const Projects = () => {
                 </div>
                 <p className="project-title">GifExpertApp</p>
                 <p className="subtitle">Ver Descripción</p>
-              </a>
+              </Link>
               <div className="card-content">
                 <p className="project-description">
                   Express con EJS | Turso SQL
