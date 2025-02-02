7642f087654ed998926380b9de676d25f8860d18
Federico Isa <fedeisa195@gmail.com>
2025-02-02 18:27:17 -0300

    update 16

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import  Portfolio  from './portfolio';
+import { Portfolio } from './portfolio';
 import {BrowserRouter as Router} from 'react-router-dom'; 
 
 import './Styles.css';
